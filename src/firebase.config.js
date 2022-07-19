575bd1a3ebc9300ef697a1209ab08ef340acd7dd
Jacqueline-Kelly <jacqueline.kelly.2016@gmail.com>
2022-07-18 17:10:47 -0700

    First commit

---
+++ src/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import {getFirestore} from 'firebase/firestore'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDEKqrqmiQ--aLlqrIDL-6OYSfupBjFEio",
+  authDomain: "housing-app-dd5ea.firebaseapp.com",
+  projectId: "housing-app-dd5ea",
+  storageBucket: "housing-app-dd5ea.appspot.com",
+  messagingSenderId: "693353697781",
+  appId: "1:693353697781:web:05efa1cb6d7dc8045f50bc",
+  measurementId: "G-CK0HYR71PN"
+};
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+export const db = getFirestore()
